c9cc81d0c1bd5a4ec98d325dff7266a1939b84e9
Sam Bao <sam.b5687@gmail.com>
2014-10-09 11:58:57 -0600

    Added test for question image size

--- LortAnswersTestTest/src/com/github/cmput301f14t11/teamlort/test/QuestionsTest.java
+++ LortAnswersTestTest/src/com/github/cmput301f14t11/teamlort/test/QuestionsTest.java
@@ -85,4 +85,13 @@ public class QuestionsTest extends TestCase {
 		assertTrue("getAnswer() did not return list with answer", list1.get(0) == answer);
 	}
 	
+	public void testQUEPIC() 
+54 	{ 
+55 		Question question = new Question(); 
+56 		private Bitmap image = new Bitmap();//this is not how bitmap is set up,more work needs to be done to make a proper bitmap 
+57 		question.addpic(i); 
+58 		assertTrue("picture file size too large",question.getpic().getAllocationByteCount () <= 64); 
+59 	} 
+
+	
 }

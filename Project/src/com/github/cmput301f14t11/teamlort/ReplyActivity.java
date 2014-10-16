eecc6ddf867f6b7bf62fe07cfa50f4711ed5f04b
sam9116 <sam.b5687@gmail.com>
2014-10-16 12:44:52 -0600

    added reply activity for UI mockup

---
+++ Project/src/com/github/cmput301f14t11/teamlort/ReplyActivity.java
@@ -0,0 +1,16 @@
+package com.github.cmput301f14t11.teamlort;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+public class ReplyActivity extends AppBaseActivity{
+
+	@Override
+	@SuppressLint("InflateParams")
+	protected void onCreate(Bundle inState) {
+		// TODO Auto-generated method stub
+		super.onCreate(inState);
+		setContentView(R.layout.activity_reply);
+	}
+	
+}

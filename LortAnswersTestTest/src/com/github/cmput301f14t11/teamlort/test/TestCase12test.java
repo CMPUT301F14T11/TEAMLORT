6d90245ed2986db78d06a79194bee6f75085265a
Sam Bao <sam.b5687@gmail.com>
2014-10-17 00:27:30 -0600

    Create TestCase12test.java

---
+++ LortAnswersTestTest/src/com/github/cmput301f14t11/teamlort/test/TestCase12test.java
@@ -0,0 +1,19 @@
+package com.github.cmput301f14t11.teamlort.test;
+
+import junit.framework.TestCase;
+
+
+public class UseCase12Test extends TestCase 
+{  
+	//yo elvis thanks buddy!
+	public void testUpvote()
+	{
+		String username = "SIVLEOL";
+		Answer answer = new Answer();
+		assertTrue("question initialized with more than 0 upvotes", answer.getScore() == 0);
+		answer.upvote(username);
+		assertTrue("Upvote did not change score", answer.getScore() == 1);
+		answer.upvote(username);
+		assertTrue("Same user managed to upvote same question twice", answer.getScore() == 1);
+	}
+}

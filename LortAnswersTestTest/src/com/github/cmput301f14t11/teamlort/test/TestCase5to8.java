f910da25ba240e340ad3965a5b0fbd548912f1ea
IrisAmp - Laptop <brandon.d.yue@gmail.com>
2014-10-17 13:12:41 -0600

    Edited the master UML and added test cases
    Test cases aren't implemented yet.

---
+++ LortAnswersTestTest/src/com/github/cmput301f14t11/teamlort/test/TestCase5to8.java
@@ -0,0 +1,33 @@
+package com.github.cmput301f14t11.teamlort.test;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+public class TestCase5to8
+extends TestCase
+{
+	public void testSanity()
+	{
+		Assert.assertEquals(0, 0);
+	}
+	
+	public void testCaseFive()
+	{
+		PersistentDataManager manager = PersistentDataManager.getInstance();
+	}
+	
+	public void testCaseSix()
+	{
+		
+	}
+	
+	public void testCaseSeven()
+	{
+		
+	}
+	
+	public void testCaseEight()
+	{
+		
+	}
+}
--- Project/src/com/github/cmput301f14t11/teamlort/ReplyActivity.java
+++ Project/src/com/github/cmput301f14t11/teamlort/ReplyActivity.java
@@ -1,12 +1,9 @@
 package com.github.cmput301f14t11.teamlort;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 public class ReplyActivity extends AppBaseActivity{
 
-	@Override
-	@SuppressLint("InflateParams")
 	protected void onCreate(Bundle inState) {
 		// TODO Auto-generated method stub
 		super.onCreate(inState);

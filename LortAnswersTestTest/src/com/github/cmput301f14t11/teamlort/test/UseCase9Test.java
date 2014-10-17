c5e26bbc27997828df959726d4750a9e67c23f7f
Sam Bao <sam.b5687@gmail.com>
2014-10-16 23:57:11 -0600

    seperated and rewrote testcase for testcase 9

---
+++ LortAnswersTestTest/src/com/github/cmput301f14t11/teamlort/test/UseCase9Test.java
@@ -0,0 +1,35 @@
+package com.github.cmput301f14t11.teamlort.test;
+
+import junit.framework.TestCase;
+
+
+public class UseCase9Test extends TestCase 
+{  
+	public void testSort()
+	{
+		public static final int CONSTANT_IMAGE = 1;
+		String title = "testing";
+		ArrayList<Question> questionlist;
+		for (int i =0; x<=9; x++)
+		{
+			if(i%2 == 0)
+			{
+				Bitmap bitmap = Bitmap.createBitmap(400, 400, Bitmap.Config.ARGB_8888 ); 
+ 				Question question = new Question(title); 
+ 				question.addImage(bitmap); 
+			}
+			questionlist.add(singlequestion);
+		}
+
+  	questionlist.sort(CONSTANT_IMAGE);
+
+		for(int i=1;i<questionlist.size()-1;i++)
+		{
+			if(questionlist.get(i-1).getpic()!=questionlist.get(i).getpic()&&questionlist.get(i).getpic()!=questionlist.get(i+1).getpic())
+			{
+				fail("list not properly sorted by image");
+			}
+			
+		}); 
+	}
+}

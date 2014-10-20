2f390642a320971d7f9fee0ddb1726c6e684d625
sam9116 <sam.b5687@gmail.com>
2014-10-20 14:36:56 -0600

    connected reply activity to function activity via buttons

--- Project/src/com/github/cmput301f14t11/teamlort/QuestionViewActivity.java
+++ Project/src/com/github/cmput301f14t11/teamlort/QuestionViewActivity.java
@@ -1,6 +1,9 @@
 package com.github.cmput301f14t11.teamlort;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class QuestionViewActivity
 extends AppBaseActivity
@@ -10,5 +13,15 @@ extends AppBaseActivity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_question_view);
+		Button reply = (Button)findViewById(R.id.reply_button);
+		
+		reply.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+            	Intent intent = new Intent(QuestionViewActivity.this,ReplyActivity.class);
+            	startActivity(intent);
+            }
+        });
 	}
+	
 }
--- Project/src/com/github/cmput301f14t11/teamlort/ReplyActivity.java
+++ Project/src/com/github/cmput301f14t11/teamlort/ReplyActivity.java
@@ -1,13 +1,40 @@
 package com.github.cmput301f14t11.teamlort;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class ReplyActivity extends AppBaseActivity{
-
+	
+	
+	
 	protected void onCreate(Bundle inState) {
 		// TODO Auto-generated method stub
 		super.onCreate(inState);
 		setContentView(R.layout.activity_reply);
+		//http://stackoverflow.com/questions/7873480/android-one-onclick-method-for-multiple-buttons
+		Button ok = (Button)findViewById(R.id.replyok);
+		Button cancel=(Button)findViewById(R.id.replycancel);
+		OnClickListener onClickListener = new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				if(v.getId() == R.id.replyok)
+				{
+					finish();
+				}
+				else if(v.getId() == R.id.replycancel);
+				{
+					finish();
+				}
+			}
+			
+		};
+		ok.setOnClickListener(onClickListener);
+		cancel.setOnClickListener(onClickListener);
+		
 	}
 	
 }

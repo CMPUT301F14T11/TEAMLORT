19eb69a61bd645bdba47f3d4777d59cea0511c55
sam9116 <sam.b5687@gmail.com>
2014-10-23 16:25:55 -0600

    implementing view: in progress

--- Project/src/com/github/cmput301f14t11/teamlort/HomeActivity.java
+++ Project/src/com/github/cmput301f14t11/teamlort/HomeActivity.java
@@ -1,17 +1,27 @@
 package com.github.cmput301f14t11.teamlort;
 
+
+
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
 
 
 public class HomeActivity
 extends AppBaseActivity {
-
+	
+	ListView questionlistview;
+	private RelativeLayout tasklayout;
+	static ArrayAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        questionlistview = (ListView)tasklayout.findViewById(R.id.expandableListView1);
     }
 
     @Override

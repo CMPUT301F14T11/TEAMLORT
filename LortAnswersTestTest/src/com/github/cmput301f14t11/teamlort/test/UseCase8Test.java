f1c99ef91c5b5c8b2ab306710caac5697132e153
Sam Bao <sam.b5687@gmail.com>
2014-10-23 12:05:32 -0600

    attempt to address feedback

--- LortAnswersTestTest/src/com/github/cmput301f14t11/teamlort/test/UseCase8Test.java
+++ LortAnswersTestTest/src/com/github/cmput301f14t11/teamlort/test/UseCase8Test.java
@@ -9,9 +9,12 @@ public class UseCase8Test extends TestCase
 	 	{  
 			String title = "testsize";
 	 		Question question = new Question(title); 
-	 		Bitmap bitmap = Bitmap.createBitmap(400, 400, Bitmap.Config.ARGB_8888 ); 
+	 		Bitmap bitmap = Bitmap.createBitmap(4000, 4000, Bitmap.Config.ARGB_8888 ); 
 	 		question.addImage(bitmap); 
-	 		assertTrue("picture file size too large",question.getpic().getAllocationByteCount () <= 64);  
+	 		// attempt to address feedback
+	 		//assertTrue("picture file size too large",question.getpic().getAllocationByteCount () <= 64);  
+	 		assertTrue("picture rejected due to filesize",question.getpic() == null);
+	 		
 	 	}  
 
 }

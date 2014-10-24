3f85feedec19e1b80cb3452b336d95c9e0bb8b66
HANG PENG <hpeng2@ualberta.ca>
2014-10-24 13:17:55 -0600

    adding UI test in use case 14

--- LortAnswersTestTest/src/com/github/cmput301f14t11/teamlort/test/UseCase13Through16Test.java
+++ LortAnswersTestTest/src/com/github/cmput301f14t11/teamlort/test/UseCase13Through16Test.java
@@ -2,6 +2,10 @@ package com.github.cmput301f14t11.teamlort.test;
 
 import java.util.ArrayList;
 
+import android.widget.TextView;
+
+import com.github.cmput301f14t11.teamlort.QuestionViewActivity;
+
 import junit.framework.TestCase;
 
 public class UseCase13Through16Test extends TestCase {
@@ -57,6 +61,9 @@ public class UseCase13Through16Test extends TestCase {
 		PersistentDataManager dataManager = new PersistentDataManager();
 		DataController inputManager = new DataController();
 		
+		QuestionViewActivity activity = (QuestionViewActivity) getAcetivity();
+		TextView tv = (TextView)activity.findViewById(R.id.textView3);
+		
 		Question question1 = new Question("Q1");
 		Question question2 = new Question("Q2");
 		Question question3 = new Question("Q3");
@@ -65,6 +72,9 @@ public class UseCase13Through16Test extends TestCase {
 		Answer answer3 = new Answer("A2Q1");
 		Answer answer4 = new Answer("A3Q1");
 		
+		assertEquals(tv.getText().toString(), 
+				String.valueOf(dataManager.countAnswer(question1)) );
+		
 		question1.addAnswer(answer1);
 		question2.addAnswer(answer2);
 		question1.addAnswer(answer3);
@@ -72,15 +82,26 @@ public class UseCase13Through16Test extends TestCase {
 		inputManager.addQuestion(question1);
 		inputManager.addQuestion(question2);
 		inputManager.addQuestion(question3);
-		// And now users should see the number of answers for each question in
+		// And now users should see the nsumber of answers for each question in
 		// the question detail screen.
 		assertFalse("wrong number of answers for question1",
 				dataManager.countAnswer(question1) != 3);
+		assertEquals(tv.getText().toString(), 
+				String.valueOf(dataManager.countAnswer(question1)) );
+		
 		assertFalse("wrong number of answers for question2",
 				dataManager.countAnswer(question2) != 1);
+		assertEquals(tv.getText().toString(), 
+				String.valueOf(dataManager.countAnswer(question1)) );
+		
 		assertFalse("wrong number of answers for question3",
 				dataManager.countAnswer(question3) != 0);
+		assertEquals(tv.getText().toString(), 
+				String.valueOf(dataManager.countAnswer(question1)) );
+		
+	
 	}
+	
 	//Search Answers or Questions
 	public void testCase15(){
 		//Create existed database

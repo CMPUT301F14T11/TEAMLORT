f8aaa514015b6e6d8930b2909d46228f3484312b
sam9116 <sam.b5687@gmail.com>
2014-10-23 22:45:49 -0600

    implementing homepage view

--- Project/src/com/github/cmput301f14t11/teamlort/HomeActivity.java
+++ Project/src/com/github/cmput301f14t11/teamlort/HomeActivity.java
@@ -2,6 +2,8 @@ package com.github.cmput301f14t11.teamlort;
 
 
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
@@ -15,13 +17,16 @@ extends AppBaseActivity {
 	
 	ListView questionlistview;
 	private RelativeLayout tasklayout;
-	static ArrayAdapter adapter;
+	static customadapter adapter; // since we are displaying question objects, the normal ArrayAdapter will not cut it, for right now I've modified the customer adapter I used for my assignment 1 and sticked it in here
+								  // should anyone think something else should be used instead,feel free to bring it up in group discussion
+	ArrayList<Question> listofquestions;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         questionlistview = (ListView)tasklayout.findViewById(R.id.expandableListView1);
+        adapter = new customadapter(getApplicationContext(), listofquestions);
     }
 
     @Override

82393ccd20d42a2ef36edf36dd26f799943a5317
IrisAmp - Laptop <brandon.d.yue@gmail.com>
2014-10-25 15:06:23 -0600

    Added code to the PDM. Still some work in progress.

--- Project/src/com/github/cmput301f14t11/teamlort/Model/PersistentDataManager.java
+++ Project/src/com/github/cmput301f14t11/teamlort/Model/PersistentDataManager.java
@@ -3,41 +3,143 @@ package com.github.cmput301f14t11.teamlort.Model;
 import java.io.Serializable;
 import java.net.URI;
 import java.util.ArrayList;
-import java.util.concurrent.Callable;
+import java.util.Observable;
+import java.util.Observer;
 
 import com.github.cmput301f14t11.teamlort.Question;
 
 public class PersistentDataManager
+extends Observable
 {
 	static private PersistentDataManager managerInstance = new PersistentDataManager();
+	public ArrayList<Observer> observers;
 	
 	static public enum SORT_METHOD
 	{
 		BY_DATE,
 		BY_SCORE,
-		BY_PICTURE,
-		BY_REPLIES
+		BY_PICTURE
 	}
 	
-	static PersistentDataManager getInstance()
+	public static PersistentDataManager getInstance()
 	{
 		return managerInstance;
 	}
 	
 	private PersistentDataManager() {}
 	
-	public void pushToCloud() { }
-	public void pullFromCloud() { }
-	public void addQuestion() { }
-	public Question get(Object id) { return null; } // Replace Object with datatype of ID
-	public ArrayList<Question> getAllQuestions() { return null; }
-	public ArrayList<Question> getSavedQuestions() { return null; }
-	public ArrayList<Question> getFavedQuestions() { return null; }
-	public ArrayList<Question> searchQuestions() { return null; }
-	public void sortQuestions(SORT_METHOD method) { }
-	public String getUsername() { return null; }
-	public void setUsername(String name) { }
-	public void notifyObservers() { }
+	public void pushToCloud()
+	{
+		// TODO: Research ElasticSearch
+		ElasticManager.serverQuery("Foo!");
+	}
+	
+	public void pullFromCloud()
+	{
+		// TODO: Research ElasticSearch
+		ElasticManager.serverQuery("Foo!");
+	}
+	
+	public void addQuestion(Question q)
+	{
+		data.uploadBuffer.add(q);
+		Save();
+	}
+	
+	public void getMore()
+	{
+		// TODO: Research ElasticSearch
+		ElasticManager.serverQuery("Foo!");
+	}
+	
+	public Question get(Object id) // Replace Object with datatype of ID
+	{
+		for (Question q : data.cacheQuestions)
+			if (q.getId() == id)
+			{
+				return q;
+			}
+		return null;
+	}
+	
+	public void saveQuestion(Question q)
+	{
+		if (!data.savedQuestions.contains(q))
+			data.savedQuestions.add(q);
+	}
+	
+	public void unsaveQuestion(Question q)
+	{
+		data.savedQuestions.remove(q);
+	}
+	
+	public void faveQuestion(Question q)
+	{
+		if (!data.favedQuestions.contains(q))
+			data.favedQuestions.add(q);
+	}
+	
+	public void unfaveQuestion(Question q)
+	{
+		data.favedQuestions.remove(q);
+	}
+	
+	public ArrayList<Question> getAllQuestions()
+	{
+		ArrayList<Question> result = new ArrayList<Question>();
+		
+		result.addAll(data.cacheQuestions);
+		result.addAll(data.savedQuestions);
+		result.addAll(data.favedQuestions);
+		
+		return result;
+	}
+	
+	public ArrayList<Question> getSavedQuestions()
+	{
+		return data.savedQuestions;
+	}
+	
+	public ArrayList<Question> getFavedQuestions() 
+	{
+		return data.favedQuestions;
+	}
+	
+	public ArrayList<Question> searchQuestions(String [] terms)
+	{
+		// Going to implement this as searching titles for keywords.
+		// WIP.
+		return null;
+	}
+	
+	public void sortQuestions(SORT_METHOD method)
+	{
+		switch (method)
+		{
+		case BY_DATE:
+			break;
+
+		case BY_SCORE:
+			break;
+			
+		case BY_PICTURE:
+			break;
+			
+		default:
+			break;
+			
+		}
+	}
+	
+	public String getUsername()
+	{
+		return data.userName;
+	}
+	
+	public void setUsername(String name)
+	{
+		data.userName = name;
+	}
 	
 	/*
 	 * ElasticManager -- auxiliary class to handle all server I/O
@@ -71,6 +173,14 @@ public class PersistentDataManager
 			return null;
 		}
 	}
+	private static void Save()
+	{
+		LocalManager.saveFile(PersistentDataManager.data);
+	}
+	private static void Load()
+	{
+		PersistentDataManager.data = (UserData) LocalManager.loadFile();
+	}
 	
 	/*
 	 * UserData container class that makes saving to file simpler.
@@ -84,5 +194,7 @@ public class PersistentDataManager
 		public ArrayList<Question> cacheQuestions;
 		public ArrayList<Question> savedQuestions;
 		public ArrayList<Question> favedQuestions;
+		public ArrayList<Question> uploadBuffer;
 	}
+	private static UserData data;
 }

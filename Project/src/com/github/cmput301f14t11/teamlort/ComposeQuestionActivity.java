59aad9cde93f908b83ce671811fe1b56585517c1
IrisAmp - Laptop <brandon.d.yue@gmail.com>
2014-11-03 11:27:45 -0700

    Added the Setup Code for ComposeQuestions

--- Project/src/com/github/cmput301f14t11/teamlort/ComposeQuestionActivity.java
+++ Project/src/com/github/cmput301f14t11/teamlort/ComposeQuestionActivity.java
@@ -1,70 +1,157 @@
 package com.github.cmput301f14t11.teamlort;
 
-import com.github.cmput301f14t11.teamlort.Model.PersistentDataManager;
-
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.KeyEvent;
 import android.view.View;
-import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
+import android.widget.Toast;
 
 public class ComposeQuestionActivity
 extends AppBaseActivity {
+	
+	//private Profile usrProfile;
+	
+	//private QuestionController qController;
+	//private ProfileController pController;
+	
+	private EditText titleEntry;
+	private EditText detailEntry;
+	private EditText tagEntry;
+	
+	private Button addImageButton;
+	private Button acceptButton;
+	private Button cancelButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		PersistentDataManager pdm = PersistentDataManager.getInstance();
-		final DataController dt = new DataController();
-		//final UserController us = new UserController();
 		setContentView(R.layout.activity_compose_question);
-		ImageButton accept = (ImageButton)findViewById(R.id.accept_button);
-		ImageButton cancel = (ImageButton)findViewById(R.id.cancel_button);
-		final EditText title = (EditText)findViewById(R.id.compose_title_entry);
-		final EditText content = (EditText)findViewById(R.id.compose_desc_entry);
 		
-		OnClickListener onClickListener = new OnClickListener(){
+		GetModel();
+		GetControllers();
+		GetLayoutElements();
+		AttachListeners();
+	}
 
+	/*
+	 * Auxiliary
+	 */
+	private void GetModel()
+	{
+		
+	}
+	private void GetControllers()
+	{
+		
+	}
+	private void GetLayoutElements()
+	{
+		titleEntry  = (EditText) this.findViewById(R.id.compose_title_entry);
+		detailEntry = (EditText) this.findViewById(R.id.compose_desc_entry);
+		tagEntry    = (EditText) this.findViewById(R.id.compose_tags_entry);
+		
+		addImageButton = (Button) this.findViewById(R.id.compose_add_img_button);
+		acceptButton   = (Button) this.findViewById(R.id.compose_accept_button);
+		cancelButton   = (Button) this.findViewById(R.id.compose_cancel_button);
+	}
+	private void AttachListeners()
+	{
+		titleEntry.setOnEditorActionListener(new OnEditorActionListener()
+		{	
 			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				if(v.getId() == R.id.accept_button)
+			public boolean onEditorAction(TextView arg0, int arg1, KeyEvent arg2)
+			{
+				if (arg1 == EditorInfo.IME_ACTION_NEXT)
 				{
-					//addquestion
-					dt.dataManager.addQuestion(dt.initQuestion(title.getText().toString(), content.getText().toString(), dt.dataManager.getUsername()));
-					finish();
+					ComposeQuestionActivity.this.detailEntry.requestFocus();
+					return true;
 				}
-				else if(v.getId() == R.id.cancel_button);
-				{
-					finish();
-				}
+				return false;
 			}
-			
-		};
-		accept.setOnClickListener(onClickListener);
-		cancel.setOnClickListener(onClickListener);
-
-			
+		});
+		
+		detailEntry.setOnEditorActionListener(new TextView.OnEditorActionListener()
+		{			
+			@Override
+			public boolean onEditorAction(TextView arg0, int arg1, KeyEvent arg2)
+			{
+				if (arg1 == EditorInfo.IME_ACTION_NEXT)
+				{
+					ComposeQuestionActivity.this.tagEntry.requestFocus();
+					return true;
+				}
+				return false;
+			}
+		});
+		
+		/* Probably don't need this one.
+		tagEntry.setOnEditorActionListener(new OnEditorActionListener()
+		{
+			@Override
+			public boolean onEditorAction(TextView arg0, int arg1, KeyEvent arg2)
+			{
+				
+				return false;
+			}
+		});//*/
+		
+		addImageButton.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View arg0)
+			{
+				// TODO: Send intent for content.
+			}
+		});
+		
+		acceptButton.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View arg0)
+			{
+				// Get all the information from the views.
+				String title, detail, tags;
+				
+				title = ((EditText) ComposeQuestionActivity.this.
+						findViewById(R.id.compose_title_entry))
+						.getText().toString();
+				detail = ((EditText) ComposeQuestionActivity.this.
+						findViewById(R.id.compose_desc_entry))
+						.getText().toString();
+				tags = ((EditText) ComposeQuestionActivity.this.
+						findViewById(R.id.compose_tags_entry))
+						.getText().toString();
+				
+				// Make sure the user has at least entered a title.
+				if(title.isEmpty() || title == null)
+				{
+					Toast.makeText(getApplicationContext(), 
+							"You need to enter a title!",
+							Toast.LENGTH_SHORT).show();
+					
+					ComposeQuestionActivity.this.titleEntry.requestFocus();
+					return;
+				}
+				
+				//ComposeQuestionActivity.this.qController
+				//.newQuestion(args);
+			}
+		});
+		
+		cancelButton.setOnClickListener(new View.OnClickListener()
+		{	
+			@Override
+			public void onClick(View arg0)
+			{
+				// Cancel and finish the activity.
+				ComposeQuestionActivity.this.setResult(RESULT_CANCELED);
+				ComposeQuestionActivity.this.finish();
+			}
+		});
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.compose_question, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
 }

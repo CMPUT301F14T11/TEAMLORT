232274bc762d93d4dbde9443dcf6eb835e8d4221
HANG PENG <hpeng2@ualberta.ca>
2014-12-01 09:52:58 -0700

    fix use case 21 test code

--- LortAnswersTestTest/src/com/github/cmput301f14t11/teamlort/test/UseCase21Test.java
+++ LortAnswersTestTest/src/com/github/cmput301f14t11/teamlort/test/UseCase21Test.java
@@ -1,33 +1,45 @@
 package com.github.cmput301f14t11.teamlort.test;
 
+import java.util.ArrayList;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.github.cmput301f14t11.teamlort.HomeActivity;
+import com.github.cmput301f14t11.teamlort.Model.AppCache;
+import com.github.cmput301f14t11.teamlort.Model.ElasticManager;
+import com.github.cmput301f14t11.teamlort.Model.LocalManager;
+import com.github.cmput301f14t11.teamlort.Model.ObjectFactory;
+import com.github.cmput301f14t11.teamlort.Model.Profile;
+import com.github.cmput301f14t11.teamlort.Model.PushQueue;
+import com.github.cmput301f14t11.teamlort.Model.Question;
+
 import junit.framework.TestCase;
 
-public class UseCase21Test extends TestCase {
+public class UseCase21Test extends  ActivityInstrumentationTestCase2<HomeActivity> {
+	
+	public UseCase21Test(){
+		super(HomeActivity.class);
+	}
+	
 	public void TestSaveQuestion() {
-		LocalManager lm = LocalManager.getInstance();
-		Question question = new Question();
-		lm.saveFile(question);
-		assertEquals(question, lm.loadFile(question));
+		Question q = ObjectFactory.initQuestion("A1", "B1", "User1");
+		LocalManager.getManager().saveQuestion(q);
+		assertEquals(q, LocalManager.getManager().loadQuestions().get(0));
 	}
 	public void TestPushSavedQuestion() {
-		LocalManager lm = LocalManager.getInstance();
-		Question question = new Question();
-		ElasticManager em = ElasticManager.getInstance();
-		lm.save(question);
-		em.sendQuestion(lm.loadSavedQuestions(question));
-		assertTrue(em.serverQuery(question) == question);
+		Question question = ObjectFactory.initQuestion("A1", "B1", "User1");
+		PushQueue.getInstance().pushQuestion(question, getActivity().getApplicationContext());
+		ArrayList<Question> result = ElasticManager.getInstance().search("A1", "Title", 1);
+		assertEquals(question, result.get(0));
+		
 	}
-	public void TestNetworkController() {
-		NetworkController nc = new NetworkController();
-		//if there's wifi: assertTrue(nc.checkConnection)
-		//else: assertFalse(nc.checkConnection)
-	}
+	
 	public void TestUseCase() {
-		ElasticManager em = ElasticManager.getInstance();
-		LocalManager lm = LocalManager.getInstance();
-		Question question = new Question();
-		lm.saveFile(question);
-		em.sendQuestion(lm.loadSavedQuestions(question));
-		assertTrue(em.serverQuery(question) == question);
+		Question question = ObjectFactory.initQuestion("A1", "B1", "User1");
+		LocalManager.getManager().saveQuestion(question);
+		LocalManager.getManager().loadQuestions().get(0);
+		PushQueue.getInstance().pushQuestion(question, getActivity().getApplicationContext());
+		ArrayList<Question> result = ElasticManager.getInstance().search("A1", "Title", 1);
+		assertEquals(question, result.get(0));
 	}
 }

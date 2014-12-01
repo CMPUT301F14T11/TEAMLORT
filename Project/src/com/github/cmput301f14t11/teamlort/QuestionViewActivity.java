3627d9e89f8a67dfc2eab22e412ffbf43795ee56
SIVLEOL <ejlo@ualberta.ca>
2014-11-30 21:26:52 -0700

    Fixed import?
    ???

--- Project/src/com/github/cmput301f14t11/teamlort/QuestionViewActivity.java
+++ Project/src/com/github/cmput301f14t11/teamlort/QuestionViewActivity.java
@@ -9,6 +9,7 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
